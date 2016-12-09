2c712d64f241f7667cf837802990ad6f60a3a9c3
Brena <brendagao.lf@gmail.com>
2016-12-08 16:54:07 -0800

    add nvd3 chart for blood pressure.

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,9 +5,9 @@
         .module('21PointsApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Points', 'Preferences'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Points', 'Preferences', 'Blood', 'Chart'];
 
-    function HomeController ($scope, Principal, LoginService, $state, Points, Preferences) {
+    function HomeController ($scope, Principal, LoginService, $state, Points, Preferences, Blood, Chart) {
         var vm = this;
 
         vm.account = null;
@@ -33,7 +33,37 @@
 
             Preferences.userPreferences(function (data) {
                 $scope.preferences = data;
-            })
+            });
+
+            Blood.last30Days(function(bpReadings) {
+                $scope.bpReadings = bpReadings;
+                if (bpReadings.readings.length) {
+                    $scope.bpOptions = angular.copy(Chart.getBpChartConfig());
+                    $scope.bpOptions.title.text = bpReadings.period;
+                    $scope.bpOptions.chart.yAxis.axisLabel = "Blood Pressure";
+                    var systolics, diastolics;
+                    systolics = [];
+                    diastolics = [];
+                    bpReadings.readings.forEach(function (item) {
+                        systolics.push({
+                            x: new Date(item.date),
+                            y: item.systolic
+                        });
+                        diastolics.push({
+                            x: new Date(item.date),
+                            y: item.diastolic
+                        });
+                    });
+                    $scope.bpData = [{
+                        values: systolics,
+                        key: 'Systolic',
+                        color: '#673ab7'
+                    }, {
+                        values: diastolics,
+                        key: 'Diastolic',
+                        color: '#03a9f4'
+                    }]; }
+            });
         }
         function register () {
             $state.go('register');
